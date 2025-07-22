dacabd80a02690108cfdb8283bfac77fcef3cc0b
hoangnguyennn <nvanhoang188@gmail.com>
2025-07-22 14:37:11 +0700

    bump dependency versions

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default defineConfig([
+  {
+    extends: compat.extends(
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ),
+
+    plugins: {
+      react,
+      '@typescript-eslint': typescriptEslint
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+
+    rules: {}
+  }
+])
